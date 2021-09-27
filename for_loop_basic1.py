32f1837f9e832555ddfa3388e594bbe65b52bbeb
Chris Frederick <chrisfrederick2008@gmail.com>
2021-09-27 17:01:17 -0500

    for loop basic 1

---
+++ for_loop_basic1.py
@@ -0,0 +1,33 @@
+# for x in range(150):
+#     print(x)
+
+# for z in range(5, 1000, 5):
+#     print(z)
+
+# for a in range(1, 100):
+#     if a%5 == 0:
+#         a="Coding"
+#         print(a)
+#     elif a%10 == 0:
+#         a="Coding Dojo"
+#         print(a)
+#     else:
+#         print(a)
+
+
+# sum =0
+# for i in range(1, 500000 + 1, 2):
+#     sum+=i
+# print(sum)
+
+
+# for i in range(2018, 0, -4):
+#     print(i)
+
+
+low_num = 2
+high_num = 9
+mult = 3
+for i in range(low_num, high_num + 1):
+    if i % mult == 0:
+        print(i)
